e0e915e05744798be55b188f8d9fc2230af64682
AdonisWork <adonisdoda@gmail.com>
2020-06-13 17:53:27 -0300

    Docs

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import FlashMessageComponent, { EventEmitter } from './src/FlashMessage'
 
+/**
+ * 
+ * @param {Object} options - Flash options params
+ * @param {'NEUTRAL'|'DANGER'|'WARNING'} options.type - Enum Types For Notification
+ * @param {string} options.title - Title Flash Notification
+ * @param {string} options.desc - Description Flash Notification
+ */
+
 const showFlash = ({ type, title, desc }) => EventEmitter.emit('SHOW_FLASH', { type: type, title: title, desc: desc })
 
 export { showFlash };
+
 
 export default FlashMessageComponent;
